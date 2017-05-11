3a4194747900c4bb74d92829b7ea2734e48d3858
timbout <publicitetim@gmail.com>
2017-05-11 16:53:42 +0200

    versoin du 11 05

--- jqueryjs.js
+++ jqueryjs.js
@@ -1,3 +1,75 @@
+
+
+$(document).ready(function(){
+
+
+            $("button.off").click(function(){
+                $("p").hide();
+            });
+            
+            $("button.on").click(function(){
+                $("p").show();
+            });
+
+
+            // <!-- Les toggles: -->
+
+            $( "button.toggle" ).click (function() {
+                $( "p" ).toggle( "slow" );
+            });
+
+            $("#monImage").fadeOut(5000).delay(300).fadeIn(5000);
+
+
+
+            $("#checkAll").click(function(){
+                var magazines= $("#magazine").find(":checkbox");
+                if (this.checked) {
+                    magazines.prop("checked", true);
+                }
+                else{magazines.prop("checked", false);
+                }
+            });
+
+
+
+
+
+
+
+            // pour compter les carateres et mots de mon texte:
+
+            $("#message") . keyup(function (){
+                var nombreCaractere = $ (this).val() .length;
+                var nombreMots = $.trim( $ (this).val()).split(" ") .length;
+
+                if ( $ (this).val === "" ) {
+                    nombreMots = 1;
+                }
+                var msg = "" + nombreMots + " mot(s) |" + nombreCaractere + " caractere(s)"
+                $("#compteur").text(msg);
+
+
+                if (nombreCaractere >200) {
+                    $("#compteur").addclass("mauvais");
+                    $("#message").addclass("mauvais-message");
+                }
+                else {
+                    $("#compteur") .removeClasss ("mauvais");
+                    $("#message") .removeClass ("mauvais-message");
+                }
+            })
+
+        });
+
+
+
+
+
+
+
+
+
 function aPayerParArticle(){
     var facturations = document.querySelectorAll (".global li")
     facturations.forEach(function(facturation){
@@ -12,4 +84,13 @@ function aPayerParArticle(){
     });
 }
 
-$("calc").get('+');
+$("#checkall").click(function(){
+    var magazines= $("#magazines").find(":checkbox");
+        if ("this.checkbox") {
+            magazines.prop("checked", true);
+        }else{magazines.prop("checked", false);
+    }
+});
+
+
+
